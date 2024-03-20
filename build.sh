a00a819a4254d78f720f71cdc33da1b8de3f041c
PugzAreCute <me@pugzarecute.com>
2024-03-20 20:31:45 +0530

    ci: fi instead of done

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ else
   cd ..
 fi
 cd kernel
+echo $(pwd)
 
 BUILD_SUFFIX=""
 KVERSION=$(cat Makefile | grep -Pe "VERSION|LEVEL" | head -3 | awk '{print $3}' | paste -sd ".")
@@ -23,14 +24,14 @@ then
   echo "Reverting LMK"
   git revert 379824bb737dd658bc69cd8edb773eb3405c77a7..1ab230774f638f0fa732bed4a005493638e15cb8
   BUILD_SUFFIX="${BUILD_SUFFIX}-MIUI"
-  done
+  fi
 else
   if $LMK_TEST;
   then
   echo "BUILD CRITICAL FAIL! LMK REVERT ON AOSP!"
   exit -1
   BUILD_SUFFIX="${BUILD_SUFFIX}-AOSP"
-  done
+  fi
 fi
 
 if [ $2 == "KSU" ]; then
@@ -42,13 +43,13 @@ if [ $2 == "KSU" ]; then
 CONFIG_HAVE_KPROBES=y
 CONFIG_KPROBE_EVENTS=y' >> arch/arm64/configs/vendor/spes-perf_defconfig
   curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-  done
+  fi
 else
   if [ -d KernelSU ];
   then
   echo "BUILD CRITICAL FAIL! KSU ON NOSU!"
   exit -1
-  done
+  fi
   BUILD_SUFFIX="${BUILD_SUFFIX}-NOSU"
 fi
 
